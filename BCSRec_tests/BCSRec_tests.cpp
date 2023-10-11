e136ab41606d820ea3e51dd4c5405a1e19b81ae9
TaoBoyce <tao.z.boyce@gmail.com>
2023-10-11 19:41:03 -0400

    Added tests for getArea

--- BCSRec_tests/BCSRec_tests.cpp
+++ BCSRec_tests/BCSRec_tests.cpp
@@ -2,6 +2,7 @@
 #include "CppUnitTest.h"
 
 extern "C" int getPerimeter(int*, int*);
+extern "C" int getArea(int*, int*);
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -27,6 +28,24 @@ namespace BCSRectests
 			int inB = 1;
 			Result = getPerimeter(&inA, &inB);
 			Assert::AreEqual(6, Result);
+		}
+
+		TEST_METHOD(getAreaFunctionality1)
+		{
+			int Result;
+			int inA = 1;
+			int inB = 2;
+			Result = getArea(&inA, &inB);
+			Assert::AreEqual(2, Result);
+		}
+
+		TEST_METHOD(getAreaFunctionality2)
+		{
+			int Result;
+			int inA = 2;
+			int inB = 1;
+			Result = getArea(&inA, &inB);
+			Assert::AreEqual(2, Result);
 		}
 	};
 }
